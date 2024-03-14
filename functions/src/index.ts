de298b74298b6922af7e5b102b0931904b44ea3c
fuelkoy <65889813+fuelkoy@users.noreply.github.com>
2024-03-14 09:44:54 +0200

    added service account to global options

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,33 +3,36 @@ import * as dotenv from "dotenv";
 import { initializeApp } from "firebase-admin/app";
 import { onRequest } from "firebase-functions/v2/https";
 import mysql from 'mysql2/promise';
-import { Connector } from '@google-cloud/cloud-sql-connector';
+import { AuthTypes, Connector } from '@google-cloud/cloud-sql-connector';
+import { setGlobalOptions } from "firebase-functions/v2";
 
 dotenv.config({ path: "../.env" });
 
 // Initialize the Firebase Admin SDK
 initializeApp();
 
+// Set global options for all functions
+setGlobalOptions({
+	region: "europe-north1",
+	serviceAccount: process.env.CLOUD_SERVICE_ACCOUNT,
+});
+
+
 const connector = new Connector();
 const clientOpts = await connector.getOptions({
-  instanceConnectionName: 'fuelkdev:europe-north1:dev',
-  // authType: AuthTypes.IAM,
+  instanceConnectionName: process.env.CLOUD_INSTANCE_CONNECTION_NAME as string,
+  authType: AuthTypes.IAM,
 });
 const pool = await mysql.createPool({
   ...clientOpts,
-  user: process.env.CLOUD_SQL_USER,
-  password: process.env.CLOUD_SQL_PASSWORD,
+  user: "functionsServiceAccount",
   database: process.env.CLOUD_SQL_DATABASE,
 });
 
-// await pool.end();
-// connector.close();
-
 export const helloWorld = onRequest(async (request, response) => {
-  response.send("Hello from Firebase!");
-
   const conn = await pool.getConnection();
   const [result] = await conn.query(`SELECT NOW();`);
-  console.log(123);
   console.table(result); // prints returned time value from server
+
+  response.send("Hello from Firebase v2!");
 });
